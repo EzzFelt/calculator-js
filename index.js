a7610d38bd287bf8cc35acb59d0e3c401f47c370
Enzo Feltrin <enzobrexo@gmail.com>
2025-02-08 00:29:15 -0300

    Adicionando a função que copia o resultado do input da calculadora

--- index.js
+++ index.js
@@ -51,3 +51,16 @@ function calculate() {
 
   resultInput.classList.remove("error")
 }
+
+// Função que copia o input da calculadora, e mostra as mensagens de copiado
+document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
+    const button = ev.currentTarget
+    if (button.innerText === "Copy") {
+      button.innerText = "Copied!"
+      button.classList.add("success")
+      navigator.clipboard.writeText(resultInput.value)
+    } else {
+      button.innerText = "Copy"
+      button.classList.remove("success")
+    }
+  })
