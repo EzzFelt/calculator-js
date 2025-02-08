bc9eee113ae740981e8106414b9ddabf239f9f21
Enzo Feltrin <enzobrexo@gmail.com>
2025-02-08 00:15:22 -0300

    Criando o evento para os botões da calculadora

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const root = document.querySelector(":root")
 const input = document.getElementById("input")
 const resultInput = document.getElementById("result")
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+// Para cada caractere da calculadora que ocorrer um click, o input receberá o valor com o evento acionado
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+    charKeyBtn.addEventListener("click", function () {
+      const value = charKeyBtn.dataset.value
+      input.value += value
+    })
+  })
